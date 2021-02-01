2c44cecd1591c38fe3c7fca7051b2f703a2d374d
Kenchu123 <k88913n@gmail.com>
2021-02-01 18:11:17 +0800

    add logo

--- client/src/components/appBar.js
+++ client/src/components/appBar.js
@@ -5,7 +5,7 @@ import Bar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
-// import IconButton from "@material-ui/core/IconButton";
+import IconButton from "@material-ui/core/IconButton";
 // import MenuIcon from "@material-ui/icons/Menu";
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
 import Slide from "@material-ui/core/Slide";
@@ -19,9 +19,14 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
   },
   menuButton: {
-    marginRight: theme.spacing(2),
+    // marginLeft: theme.spacing(1),
+  },
+  menuIcon: {
+    width: theme.spacing(4),
+    height: theme.spacing(4),
   },
   title: {
+    // marginLeft: theme.spacing(2),
     flex: 1,
     cursor: "pointer",
   },
@@ -88,14 +93,17 @@ export default function AppBar() {
       <HideOnScroll>
         <Bar position="fixed" color="primary">
           <Toolbar style={{ minHeight: "56px" }}>
-            {/* <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton> */}
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+            >
+              <img
+                className={classes.menuIcon}
+                src="/NMLab2020Fall-Final/logo.png"
+              />
+            </IconButton>
             <Typography
               variant="h1"
               className={classes.title}
